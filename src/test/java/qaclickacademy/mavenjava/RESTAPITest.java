c195d6957b99d079f2cb7d4673835e4db0497c99
AshokVenkatesh <Ashokvenkatesh.kbv@gmail.com>
2022-02-15 13:01:47 +0530

    This is commit from Asian Timezone Guy

--- src/test/java/qaclickacademy/mavenjava/RESTAPITest.java
+++ src/test/java/qaclickacademy/mavenjava/RESTAPITest.java
@@ -4,15 +4,16 @@ import org.testng.annotations.Test;
 
 public class RESTAPITest {
 	@Test
-	public void postJira()
-	{
+	public void postJira() {
 		System.out.println("postJira");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
+
 	}
-	
+
 	@Test
-	public void deleteTwitter()
-	{
-		System.out.println("deleteTwitter");	
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
 	}
 
 }
